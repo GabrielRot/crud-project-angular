7feef2f682eda7b79bd79f47911a2a6453061eb2
Gabriel Teixeira <xcross341@gmail.com>
2025-11-15 15:15:39 -0300

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { SignUp } from './pages/login/sign-up/sign-up';
+import { SignIn } from './pages/login/sign-in/sign-in';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    children: [
+      { path: 'sign-in', component: SignIn },
+      { path: '', redirectTo: 'sign-in', pathMatch: 'full' }
+    ]
+  },
+  { path: 'sign-up', component: SignUp, pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' },
+];
