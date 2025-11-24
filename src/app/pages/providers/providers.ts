8e8ec125dc2920769ca1bcffccc4bbb2408ccc0a
Gabriel Teixeira <xcross341@gmail.com>
2025-11-23 22:17:03 -0300

    Creating about page and fixing global css

--- src/app/pages/providers/providers.ts
+++ src/app/pages/providers/providers.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { Technocard } from "../../components/cards/technocard/technocard";
+import { AgGridAngular } from 'ag-grid-angular';
 
 @Component({
   selector: 'app-providers',
-  imports: [Technocard],
+  imports: [AgGridAngular],
   templateUrl: './providers.html',
   styleUrl: './providers.scss',
 })
